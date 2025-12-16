ea42d33c37c7bab7a0fb4c061f43b9e1bbc297ce
NareshM1702 <risingphoenix785@gmail.com>
2025-12-16 14:51:58 +0530

    Fix commandString variable usage in service_sshd.sh

--- source/scripts/init/service.d/service_sshd.sh
+++ source/scripts/init/service.d/service_sshd.sh
@@ -208,17 +208,17 @@ do_start() {
              commandString="$commandString -p [$CM_IPV4]:22"
         fi
     elif [ "$BOX_TYPE" = "WNXL11BWL" ]; then
-	CM_IP=`ip -4 addr show dev $CMINTERFACE  scope global | awk '/inet/{print $2}' | cut -d '/' -f1 | head -n1`
+	    CM_IP=`ip -4 addr show dev $CMINTERFACE  scope global | awk '/inet/{print $2}' | cut -d '/' -f1 | head -n1`
         if [ ! -z $CM_IP ]; then
-	        commandString="commandString -p [$CM_IP]:22"
+	        commandString="$commandString -p [$CM_IP]:22"
 	    fi
         CM_IPv6=`ip -4 addr show dev wwan0  scope global | awk '/inet/{print $2}' | cut -d '/' -f1 | head -n1`
 	    if [ ! -z $CM_IPv6 ]; then
-            commandString="commandString -p [$CM_IPV6]:22"
+            commandString="$commandString -p [$CM_IPv6]:22"
 	    fi
 	    CM_IPv4=`ip -4 addr show dev wwan0  scope global | awk '/inet/{print $2}' | cut -d '/' -f1 | head -n1`
 	    if [ ! -z $CM_IPv4 ]; then
-            commandString="commandString -p [$CM_IPv4]:22"
+            commandString="$commandString -p [$CM_IPv4]:22"
         fi
     else
         CM_IP=""
@@ -267,8 +267,8 @@ do_start() {
       echo_t "utopia: dropbear could not be started on erouter0 IPv6 interface."
     fi
    else
-       if  ([ "$MANUFACTURE" = "Technicolor" ] || [ "$MODEL_NUM" = "SG417DBCT" ] || [ "$BOX_TYPE" = "WNXL11BWL"]) ; then
-	      echo dropbear -E -s -K 60 -b /etc/sshbanner.txt ${commandString} -r ${DROPBEAR_PARAMS_1} -r ${DROPBEAR_PARAMS_2} -a -P ${PID_FILE}
+       if  ([ "$MANUFACTURE" = "Technicolor" ] || [ "$MODEL_NUM" = "SG417DBCT" ] || [ "$BOX_TYPE" = "WNXL11BWL" ]) ; then
+	      echo_t "dropbear -E -s -K 60 -b /etc/sshbanner.txt ${commandString} -r ${DROPBEAR_PARAMS_1} -r ${DROPBEAR_PARAMS_2} -a -P ${PID_FILE}"
           dropbear -E -s -b /etc/sshbanner.txt $commandString -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 -a -P $PID_FILE -K 60 $USE_DEVKEYS 2>>$CONSOLEFILE
        elif [ "$BOX_TYPE" = "SCER11BEL" -a "$LANIPV6Support" = "true" ]; then
               dropbear -E -s -b /etc/sshbanner.txt $commandString -r $DROPBEAR_PARAMS_1 -r $DROPBEAR_PARAMS_2 -a -P $PID_FILE -K 60 $USE_DEVKEYS 2>>$CONSOLEFILE
