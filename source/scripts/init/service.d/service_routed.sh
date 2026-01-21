64632d7d4907edd1017b2bf4a01ade7b22d3e1c2
aj970 <akshaya_j@comcast.com>
2026-01-21 10:29:15 +0000

    Trigger zebra restart once LAN started
    Signed-off-by: aj970 <akshaya_j@comcast.com>

--- source/scripts/init/service.d/service_routed.sh
+++ source/scripts/init/service.d/service_routed.sh
@@ -68,7 +68,7 @@ case "$1" in
        status=$(sysevent get lan-status)
        if [ "$status" == "started" ]; then
            service_routed start
-           service_routed radv-restart
+           service_routed radv-start
        elif [ "$status" == "stopped" ]; then
            # As per Sky requirement, radvd should run with ULA prefix though the wan-status is down
            if [ "$BOX_TYPE" != "HUB4" ] && [ "$BOX_TYPE" != "SR300" ] && [ "$BOX_TYPE" != "SE501" ] && [ "$BOX_TYPE" != "SR213" ] && [ "$BOX_TYPE" != "WNXL11BWL" ] && [ "$LANULASupport" != "true" ]; then
