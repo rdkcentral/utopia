51d0a622e51a5d8aaf4bca4cbadd579947b054b9
aj970 <akshaya_j@comcast.com>
2026-02-11 05:14:29 +0000

    Fix for zebra restart issue in XLE
    Signed-off-by: aj970 <akshaya_j@comcast.com>

--- source/scripts/init/service.d/service_routed.sh
+++ source/scripts/init/service.d/service_routed.sh
@@ -85,8 +85,9 @@ case "$1" in
        if [ "$status" == "started" ]; then
            log INFO "LAN status started. Starting routed service."
            service_routed start
-	       if [ "$BOX_TYPE" == "WNXL11BWL" ]; then
-		       service_routed radv-start
+	        if [ "$BOX_TYPE" == "WNXL11BWL" ]; then
+		        service_routed radv-start
+            fi
        elif [ "$status" == "stopped" ]; then
            # As per Sky requirement, radvd should run with ULA prefix though the wan-status is down
            if [ "$BOX_TYPE" != "HUB4" ] && [ "$BOX_TYPE" != "SR300" ] && [ "$BOX_TYPE" != "SE501" ] && [ "$BOX_TYPE" != "SR213" ] && [ "$BOX_TYPE" != "WNXL11BWL" ] && [ "$LANULASupport" != "true" ]; then
