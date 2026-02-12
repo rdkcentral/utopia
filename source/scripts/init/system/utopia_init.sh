241b9eb94af103d731d4bf285be1c23fa3e2a5ec
NareshM1702 <risingphoenix785@gmail.com>
2026-02-12 14:48:37 +0530

    Update utopia_init.sh

--- source/scripts/init/system/utopia_init.sh
+++ source/scripts/init/system/utopia_init.sh
@@ -262,6 +262,23 @@ if [ "$SYSCFG_LAN_DOMAIN" == "utopia.net" ]; then
    syscfg commit
 fi
 
+#Change devicetype on firmware udgrade
+DEVICETYPE_MIGRATE="$(syscfg get devicetype_migrate)"
+echo "$DEVICETYPE_MIGRATE"
+if [ -z "$DEVICETYPE_MIGRATE" ]; then
+  CURRENT_DEVICETYPE="$(syscfg get DeviceType)"
+  echo "[utopia] Devicetype is $CURRENT_DEVICETYPE"
+  if [ "$CURRENT_DEVICETYPE" != "PROD" ]; then
+    echo "setting DeviceType to PROD"
+    syscfg set DeviceType "PROD"
+    syscfg commit
+  else
+    echo "DeviceType is already PROD, no change needed"
+  fi
+  syscfg set devicetype_migrate "1"
+  syscfg commit
+fi
+
 if [ -f $SYSCFG_OLDBKUP_FILE ];then
 	rm -rf $SYSCFG_OLDBKUP_FILE
 fi
