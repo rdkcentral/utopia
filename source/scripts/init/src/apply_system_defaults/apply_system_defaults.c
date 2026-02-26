9d997ec07aaa8a9fb7fbdaab27ff358ec53aebef
abhishek-kumaracee2 <abhishek_kumaracee2@comcast.com>
2026-02-26 16:22:09 +0000

    RDKB-63166: Update device.properties file during runtime (#233)
    Reason for change: for onestack support
    Test Procedure: NA
    Risks: Low
    Priority: Medium
    Signed-off-by: abhishek_kumaracee2@comcast.com
    
    Signed-off-by: abhishek_kumaracee2@comcast.com

--- source/scripts/init/src/apply_system_defaults/apply_system_defaults.c
+++ source/scripts/init/src/apply_system_defaults/apply_system_defaults.c
@@ -60,6 +60,7 @@
 #include  "safec_lib_common.h"
 
 #ifdef _ONESTACK_PRODUCT_REQ_
+#include <onestack_init.h>
 #include <devicemode.h>
 #endif
 
@@ -3655,6 +3656,10 @@ static void getPartnerIdWithRetry(char* buf, char* PartnerID)
    }
 
    sysevent_close(global_fd, global_id);
+
+#if defined (_ONESTACK_PRODUCT_REQ_)
+   onestackutils_update_devprops();
+#endif
 
    return(0);
 }
