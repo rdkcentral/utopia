661103a0fca04be1e5c5e5cf033a0232fc982702
usi096 <umasankar.srinivasan@sky.uk>
2026-06-24 11:31:15 +0100

    SHARMAN-4090 WAN SSH is successful without authentication
    Reason for change: Creating a firewall rule to block SSH using WAN IP from LAN client
    
    Test Procedure: NA
    
    Risks: LOW
    
    Priority: P1
    
    Signed-off-by: usi096 <umasankar.srinivasan@sky.uk>

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -10770,7 +10770,6 @@ static int do_block_lan_access_to_wan_ssh(FILE *fp)
 {
    FIREWALL_DEBUG("Entering do_block_lan_access_to_wan_ssh\n");
    fprintf(fp, "-I INPUT 1 -i %s -d %s -p tcp --dport 10022 -j REJECT\n", lan_ifname, current_wan_ipaddr);
-   fprintf(fp, "-I INPUT 2 -i %s -d %s -p tcp --dport 10022 -j ACCEPT\n", lan_ifname, lan_ipaddr);
    FIREWALL_DEBUG("Exiting do_block_lan_access_to_wan_ssh\n");
    return(0);
 }
