08becfa421eeed0075ffc1dbfae4d3f981f96214
Sriraaman C <sriraaman33@gmail.com>
2026-09-21 15:17:00 +0530

    script logging

--- source/scripts/init/service.d/log_capture_path.sh
+++ source/scripts/init/service.d/log_capture_path.sh
@@ -36,4 +36,6 @@ mkfifo "$SCRIPT_TRACE_FIFO"
 tee -a "$CONSOLEFILE" < "$SCRIPT_TRACE_FIFO" >> "$SCRIPT_TRACE_LOG" &
 exec > "$SCRIPT_TRACE_FIFO" 2>&1
 rm -f "$SCRIPT_TRACE_FIFO"
+PS4='+ pid=$$ script=$0 line=$LINENO: '
+set -x
 
